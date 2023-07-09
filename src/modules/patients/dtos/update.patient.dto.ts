1f3e22c1f7e266bac8ce0461cab0d22e270c80f6
Diogo Faria <diogofaria073@gmail.com>
2023-07-09 20:01:09 -0300

    Add patient basic crud and add .eslintrc.json configuration (#2)

---
+++ src/modules/patients/dtos/update.patient.dto.ts
@@ -0,0 +1,64 @@
+import { ApiProperty, ApiTags } from '@nestjs/swagger'
+import { IsBoolean, IsNotEmpty, IsString } from 'class-validator'
+
+@ApiTags('Patients')
+export class UpdatePatientDto {
+  @ApiProperty({ example: 'cuuid' })
+  @IsNotEmpty()
+  @IsString()
+  id: string
+
+  @ApiProperty({ example: 'José Belini da Silva Cruz' })
+  @IsNotEmpty()
+  @IsString()
+  name: string
+
+  @ApiProperty({ example: '1972-01-01' })
+  @IsNotEmpty()
+  @IsString()
+  birthDate: string
+
+  @ApiProperty({ example: '089.887.256-85' })
+  @IsNotEmpty()
+  @IsString()
+  documentNumber: string
+
+  @ApiProperty({ example: 'Rua do Sossego' })
+  @IsNotEmpty()
+  @IsString()
+  street: string
+
+  @ApiProperty({ example: '158' })
+  @IsNotEmpty()
+  @IsString()
+  number: string
+
+  @ApiProperty({ example: '20A' })
+  @IsString()
+  complement: string
+
+  @ApiProperty({ example: 'Floresta' })
+  @IsNotEmpty()
+  @IsString()
+  neighborhood: string
+
+  @ApiProperty({ example: '31015-260' })
+  @IsNotEmpty()
+  @IsString()
+  postalCode: string
+
+  @ApiProperty({ example: 'Belo Horizonte' })
+  @IsNotEmpty()
+  @IsString()
+  city: string
+
+  @ApiProperty({ example: 'Minas Gerais' })
+  @IsNotEmpty()
+  @IsString()
+  state: string
+
+  @ApiProperty({ example: true })
+  @IsNotEmpty()
+  @IsBoolean()
+  isActive: boolean
+}
